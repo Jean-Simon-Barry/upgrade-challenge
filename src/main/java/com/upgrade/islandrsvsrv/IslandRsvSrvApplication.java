ac1e715cd32f78f1e94a18c0ba2cab0dd951ae73
Jean-Simon-Barry <jean.simon.barry@gmail.com>
2019-10-11 14:03:36 -0400

    Initial setup using spring initialzr

---
+++ src/main/java/com/upgrade/islandrsvsrv/IslandRsvSrvApplication.java
@@ -0,0 +1,13 @@
+package com.upgrade.islandrsvsrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IslandRsvSrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IslandRsvSrvApplication.class, args);
+	}
+
+}
