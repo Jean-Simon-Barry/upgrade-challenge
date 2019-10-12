f7bed9fa9cea1a184e3f77d942196a7c39974929
Jean-Simon-Barry <jean.simon.barry@gmail.com>
2019-10-12 16:14:12 -0400

    Integration tests for database using test containers Small fix to use inclusive dates when querying database

--- src/main/java/com/upgrade/islandrsvsrv/repository/ReservationDAO.java
+++ src/main/java/com/upgrade/islandrsvsrv/repository/ReservationDAO.java
@@ -1,29 +1,56 @@
 package com.upgrade.islandrsvsrv.repository;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import reactor.core.publisher.Flux;
 
+import java.sql.Array;
 import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+
+import static java.time.temporal.ChronoUnit.DAYS;
 
 @RequiredArgsConstructor
 public class ReservationDAO {
 
-	private static final String GET_AVAILABILITIES_QUERY = "SELECT get_available_periods('[?,?)'::daterange)";
+	private static final String GET_AVAILABILITIES_QUERY = "SELECT get_available_periods(daterange(?, ?, '[]'))";
+	private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
 	private final JdbcTemplate jdbc;
 
 	public Flux<Period> getAvailabilities(LocalDate start, LocalDate end) {
 		return Flux.fromIterable(jdbc.query(GET_AVAILABILITIES_QUERY,
-													(rs, num) -> availabilitiesFromQuerySet(rs),
-													Date.valueOf(start),
-											Date.valueOf(start)));
+											(rs, num) -> availabilitiesFromQuerySet(rs),
+											Date.valueOf(start),
+											Date.valueOf(end)));
 	}
 
-	private static Period availabilitiesFromQuerySet(ResultSet rs) throws SQLException {
-		return Period.between(rs.getDate(0).toLocalDate(), rs.getDate(1).toLocalDate());
+	private Period availabilitiesFromQuerySet(ResultSet rs) throws SQLException {
+		String dateRangeString = rs.getString(1);
+		return parsePeriodFromDateRangeString(dateRangeString);
 	}
+
+	/**
+	 * It seems like there's no support for jdbc to resultset.getDateRange(column), so I am forced to parse the range as
+	 * a string. It's no big deal I guess but adds some complexity.
+	 * @param dateRange the daterange is of this format : '[yyyy-MM-dd,yyyy-MM-dd)'
+	 * @return a Period parsed from daterange passed as a string
+	 */
+	private Period parsePeriodFromDateRangeString(String dateRange) {
+		String[] stringDates = dateRange
+				.replace("[", "")
+				.replace(")", "")
+				.split(",");
+
+		LocalDate start = LocalDate.parse(stringDates[0], DATE_TIME_FORMATTER);
+		//remove 1 day from the end since we have exclusive end dates
+		LocalDate end = LocalDate.parse(stringDates[1], DATE_TIME_FORMATTER).minus(1, DAYS);
+		return Period.between(start, end);
+	}
+
 }
---
+++ src/test/java/com/upgrade/islandrsvsrv/repository/ReservationDAOTestIT.java
@@ -0,0 +1,90 @@
+package com.upgrade.islandrsvsrv.repository;
+
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.PostgreSQLContainer;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+
+import static java.time.temporal.ChronoUnit.DAYS;
+import static org.hamcrest.Matchers.hasItems;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(initializers = {ReservationDAOTestIT.Initializer.class})
+@SpringBootTest
+public class ReservationDAOTestIT {
+
+	private ReservationDAO reservationDAO;
+	private static final LocalDate NOW = LocalDate.now();
+	private static final LocalDate START_DATE_WINDOW = NOW;
+	private static final LocalDate END_DATE_WINDOW = NOW.plus(10, DAYS);
+
+	private static final LocalDate RESERVATION_START = START_DATE_WINDOW.plus(3, DAYS);
+	private static final LocalDate RESERVATION_END = RESERVATION_START.plus(3, DAYS);
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@ClassRule
+	public static PostgreSQLContainer postgres = new PostgreSQLContainer<>("postgres:11")
+			.withDatabaseName("island_resort_test")
+			.withUsername("upgrade")
+			.withPassword("upgrade");
+
+	@Before
+	public void setUp() {
+		reservationDAO = new ReservationDAO(jdbcTemplate);
+
+		// given
+		insertUser();
+		insertReservation(RESERVATION_START, RESERVATION_END);
+	}
+
+	@Test
+	public void testReturnsAvailabilityPeriods() {
+
+		// when
+		Flux<Period> availabilities = reservationDAO.getAvailabilities(START_DATE_WINDOW, END_DATE_WINDOW);
+
+		// then
+		StepVerifier.create(availabilities)
+				.recordWith(ArrayList::new)
+				.expectNextCount(2)
+				.consumeRecordedWith(periods -> assertThat(periods, hasItems(Period.between(START_DATE_WINDOW, RESERVATION_START.minus(1, DAYS)),
+																			 Period.between(RESERVATION_END, END_DATE_WINDOW))))
+				.verifyComplete();
+	}
+
+	private void insertReservation(LocalDate start, LocalDate end) {
+		jdbcTemplate.execute("insert into camping_reservation(camping_user_id, reservation_dates) " +
+									 "values (1, daterange(\'" + start + "\',\'" + end + "\'));");
+	}
+
+	private void insertUser() {
+		jdbcTemplate.execute("insert into camping_user(name, last_name, email) values ('js', 'js', 'js@js.com');");
+	}
+
+	static class Initializer
+			implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+		public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+			TestPropertyValues.of(
+					"spring.datasource.url=" + postgres.getJdbcUrl(),
+					"spring.datasource.username=" + postgres.getUsername(),
+					"spring.datasource.password=" + postgres.getPassword()
+			).applyTo(configurableApplicationContext.getEnvironment());
+		}
+	}
+}
