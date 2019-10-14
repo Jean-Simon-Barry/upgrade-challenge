92704421e31c965d35a07150cb1c3fba789da994
Jean-Simon-Barry <jean.simon.barry@gmail.com>
2019-10-14 18:35:52 -0400

    New logic for finding availability since old one in sql was flawed

---
+++ src/main/java/com/upgrade/islandrsvsrv/services/AvailabilityService.java
@@ -0,0 +1,53 @@
+package com.upgrade.islandrsvsrv.services;
+
+import com.upgrade.islandrsvsrv.domain.DateInterval;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.time.temporal.ChronoUnit.DAYS;
+import static java.util.stream.Collectors.toList;
+
+public class AvailabilityService {
+
+	public List<LocalDate> getAvailableDates(DateInterval requestedInterval, List<DateInterval> sortedOccupiedIntervals) {
+
+		LocalDate currentStart = requestedInterval.getStart();
+		List<LocalDate> availableDates = new ArrayList<>();
+
+		//return start1.before(end2) && start2.before(end1);
+		List<DateInterval> filteredReservations = sortedOccupiedIntervals.stream()
+				.filter(reservation -> reservation.getStart().isBefore(requestedInterval.getEnd()) &&
+						requestedInterval.getStart().isBefore(reservation.getEnd()))
+				.collect(toList());
+
+		if (filteredReservations.isEmpty()) {
+			return requestedInterval.getStart()
+					.datesUntil(requestedInterval.getEnd())
+					.collect(toList());
+		}
+
+		for (DateInterval reservationDate : filteredReservations) {
+			availableDates.addAll(currentStart
+										  .datesUntil(maximum(requestedInterval.getStart(), reservationDate.getStart()))
+										  .collect(toList()));
+			currentStart = minimum(requestedInterval.getEnd(), reservationDate.getEnd());
+		}
+
+		//get the last stretch of availabilities between the last reservation end and the requested interval end
+		availableDates.addAll(currentStart.datesUntil(requestedInterval.getEnd()).collect(toList()));
+
+		return availableDates;
+	}
+
+	private LocalDate minimum(LocalDate date1, LocalDate date2) {
+		return date1.isBefore(date2) ? date1 : date2;
+	}
+
+	private LocalDate maximum(LocalDate date1, LocalDate date2) {
+		return date1.isAfter(date2) ? date1 : date2;
+	}
+}
